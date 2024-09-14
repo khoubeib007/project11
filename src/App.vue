ac7edf5027e0603cc95da9e84dfe2cf4c37dbcff
khoubeib007 <khoubeib007@gmail.com>
2024-09-14 09:39:39 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <h1 class="title">welcome to wino papa</h1>
+  <h2>khoubeib khedri</h2>
+  
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+  
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  
+}
+.title{
+  color: red;
+}
+h2{
+  color: green
+}
+</style>
